2fcfd7c494f0109acf0e935b9cf9b73a20899eec
Marc Redemske <code@mgred.dev>
2024-06-17 10:47:57 +0200

    refactor: make everything shipable

--- .github/workflows/release.sh
+++ .github/workflows/release.sh
@@ -1,15 +1,26 @@
 #!/usr/bin/env bash
 
-host=$(uname -s)
-NAME=project_name
-TAG=${GITHUB_REF_NAME}
+NAME=$(echo "$GITHUB_REF_NAME" | awk -F- '{print $1}')
+TAG=$(echo "$GITHUB_REF_NAME" | awk -F- '{print $2}')
 VERSION=${TAG:1}
-PREFIX="${NAME}-${VERSION}"
-RULES_ARCHIVE="${NAME}-${TAG}.tar.gz"
+[[ "$NAME" == "graaljs" ]] && PROJECT_NAME="rules_graaljs" || PROJECT_NAME="toolchains_graalnodejs"
+PREFIX="${PROJECT_NAME}-${VERSION}"
+RULES_ARCHIVE="${PROJECT_NAME}-${TAG}.tar.gz"
+
+build_file=$(mktemp)
+echo "package(default_visibility = [\"//visibility:public\"])" > "$build_file"
+TARGETS="$NAME lib $build_file"
+TAR_ARGS="--absolute-names --mode=644 --owner=build --group=build --transform="s,$build_file,BUILD.bazel," --transform="s,$NAME/MODULE.bazel,MODULE.bazel," --transform="s,^,$PREFIX/,""
+if [[ "$NAME" == "graaljs" ]]; then
+  repo_file=$(mktemp)
+  sed "s/modules/rules/g" graaljs/repo.bzl > "$repo_file"
+  TARGETS="$TARGETS $repo_file"
+  TAR_ARGS="$TAR_ARGS --transform="s,$repo_file,graaljs/repo.bzl,""
+fi
 
 echo -n "build: Create Rules Archive"
-git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip >$RULES_ARCHIVE
-RULES_SHA=$(shasum -a 256 $RULES_ARCHIVE | awk '{print $1}')
+tar cvf "$RULES_ARCHIVE" $TAR_ARGS $TARGETS
+RULES_SHA=$(shasum -a 256 "$RULES_ARCHIVE" | awk '{print $1}')
 echo " ... done ($RULES_ARCHIVE: $RULES_SHA)"
 
 echo -n "build: Create Release Notes"
@@ -20,35 +31,22 @@ cat > release_notes.md <<EOF
 > [!IMPORTANT]  
 > Installation is only supported via Bzlmod!
 
-Choose from the options below and put as dependency in your `MODULE.bazel`.
+Choose from the options below and put as dependency in your \`MODULE.bazel\`.
 
 ### Install from BCR
 
 \`\`\`starlark
-bazel_dep(name = "bzlparty_project_name", version = "${VERSION}")
-\`\`\`
-
-
-### Install from Git
-
-\`\`\`starlark
-bazel_dep(name = "bzlparty_project_name")
-
-git_override(
-    module_name = "bzlparty_project_name",
-    remote = "git@github.com:bzlparty/project_name.git",
-    commit = "${GITHUB_SHA}",
-)
+bazel_dep(name = "bzlparty_${PROJECT_NAME}", version = "${VERSION}")
 \`\`\`
 
 ### Install from Archive
 
 \`\`\`starlark
-bazel_dep(name = "bzlparty_project_name")
+bazel_dep(name = "bzlparty_${PROJECT_NAME}")
 
 archive_override(
     module_name = "bzlparty_project_name",
-    urls = "https://github.com/bzlparty/project_name/releases/download/${TAG}/${RULES_ARCHIVE}",
+    urls = "https://github.com/bzlparty/modules_graaljs/releases/download/${GITHUB_REF_NAME}/${RULES_ARCHIVE}",
     strip_prefix = "${PREFIX}",
     integrity = "sha256-${RULES_SHA}",
 )
